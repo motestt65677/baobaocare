07620c66d2433d301e5d283efa07d20137bd2acb
Kok Yee Yang <yee_yang94@hotmail.com>
2018-03-13 14:51:26 +0800

    corrected different views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
   def index
+  	if current_user.nil? == false
+
     if current_user.type == "Mother"
       redirect_to mother_path(current_user)
     elsif current_user.type == "Doctor"
       redirect_to chatrooms_path
     end
+
+  end
   end
 end
