220d405ec633f4dbdf6883e8cf70fe7e2042b87e
Mori <mikomori0909@gmail.com>
2018-03-13 06:11:34 +0000

    fix sign in path

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user.type == "Mother"
-      redirect_to mother_path(current_user)
-    elsif current_user.type == "Doctor"
-      redirect_to chatrooms_path
+    if signed_in?
+      if current_user.type == "Mother"
+        redirect_to mother_path(current_user)
+      elsif current_user.type == "Doctor"
+        redirect_to chatrooms_path
+      end
     end
   end
 end
