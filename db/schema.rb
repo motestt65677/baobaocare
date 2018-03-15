2c5b6ea20f93377fcb7e1ff10e2977c11a64b231
Halah Abuateeq <halah.abuateeq@gmail.com>
2018-03-15 21:36:52 +0800

    added a chatroom_id to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315102045) do
+ActiveRecord::Schema.define(version: 20180315133410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180315102045) do
   create_table "reservations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chatroom_id"
   end
 
   create_table "timeslots", force: :cascade do |t|
