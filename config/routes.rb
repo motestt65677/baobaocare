b8a9aadee1382513e295f96f32727cd2800c0413
Kok Yee Yang <yee_yang94@hotmail.com>
2018-03-15 12:28:50 +0800

    added braintree form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
@@ -28,8 +30,8 @@ Rails.application.routes.draw do
 
   get "/doctors/:id/homepage" => "doctors#homepage", as: "doctor_profile"
   post '/doctor/:doctor_id/children/:child_id' => "chatrooms#create", as:"create_chatroom"
-  get 'payment/:chatroom_id/new' => "payments#new", as:"new_payment"
-  post 'payment/:chatroom_id/checkout' => "payments#checkout", as: "check_out"
+  get 'braintree/new' => "braintree#new"
+  post 'braintree/checkout' => "braintree#checkout", as: "check_out"
 
   mount ActionCable.server, at: '/cable'
 
