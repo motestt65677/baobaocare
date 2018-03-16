1e88ddec5a9bfe2c5ec18e7c6762ed53c2370358
Halah Abuateeq <halah.abuateeq@gmail.com>
2018-03-16 11:37:27 +0800

    added start time and end time columns to the reservations table also added manage reservations image to mothers homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315133410) do
+ActiveRecord::Schema.define(version: 20180316024615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,11 @@ ActiveRecord::Schema.define(version: 20180315133410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "chatroom_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.time "start_time"
+    t.boolean "confirmed"
+    t.time "end_time"
   end
 
   create_table "timeslots", force: :cascade do |t|
