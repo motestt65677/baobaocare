e4275f96b660faef90f96eae6f4e37ec1bea9966
motestt65677 <pkpk99887766@gmail.com>
2018-03-20 11:20:12 +0800

    uses aws for uploaders

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+
+  
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => ENV['AMAZON_KEY'],            # required
+    :aws_secret_access_key  => ENV['AMAZON_SECRET_KEY'],     # required
+    :region                 => 'ap-southeast-1'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'baobaocare'               # required
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
