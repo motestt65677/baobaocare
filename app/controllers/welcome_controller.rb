1f943b0f89338ad5e1320911b2edfaefe0e5ed0c
motestt65677 <pkpk99887766@gmail.com>
2018-04-12 15:21:26 +0800

    added chat route, sign in as a guess and direct to chatroom21

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,9 +6,11 @@ class WelcomeController < ApplicationController
       elsif current_user.type == "Doctor"
         redirect_to doctor_profile_path(current_user)
       end
-
-
     end
+  end
+  def chat
+    sign_in(User.find_by_first_name("Guest"))
+    redirect_to chatroom_path(Chatroom.find(21))
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   post "/guest" => "sessions#guest", as: "guest"
+  post "/chat" => "welcome#chat", as: "chat"
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
